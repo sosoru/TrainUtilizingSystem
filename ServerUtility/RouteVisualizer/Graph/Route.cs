74e0e6078e73025a76b242b05ab3a49d256ed19d
sosoru <siva6991@yahoo.co.jp>
2012-04-20 11:37:53 +0900

    add some utility codes
    Signed-off-by: sosoru <siva6991@yahoo.co.jp>

---
+++ ServerUtility/RouteVisualizer/Graph/Route.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RouteVisualizer.Models;
+
+using Livet;
+
+namespace RouteVisualizer.Graph
+{
+    public class Route<Tgate, Tedge>
+        where Tgate : IGate<Tgate, Tedge>
+        where Tedge : IEdge<Tgate, Tedge>
+    {
+
+        public Route()
+        {
+            this.Edges = new List<Tedge>();
+            this.Gates = new List<Tgate>();
+        }
+
+        public Route(Route<Tgate, Tedge> rt)
+        {
+            this.Edges = new List<Tedge>(rt.Edges);
+            this.Gates = new List<Tgate>(rt.Gates);
+        }
+
+        public virtual IList<Tedge> Edges { get; private set; }
+        public virtual IList<Tgate> Gates { get; private set; }
+
+    }
+
+    public class RouteFactory<Tgate, Tedge>
+        where Tgate : IGate<Tgate, Tedge>
+        where Tedge : IEdge<Tgate, Tedge>
+    {
+        public Func<Route<Tgate, Tedge>, Tedge, bool> ValidateForRouteFunc { get; private set; }
+
+        public virtual Route<Tgate, Tedge> SearchLoop(Tgate start, Tgate end)
+        {
+            var route  = new Route<Tgate, Tedge>();
+
+            _totalRoute.Clear();
+
+            recSearchLoop(route, start, end);
+
+            return _totalRoute.OrderBy(_ => _.Edges.Count)
+                                .First();
+        }
+
+        private IList<Route<Tgate, Tedge>> _totalRoute = new List<Route<Tgate, Tedge>>();
+        private void recSearchLoop(Route<Tgate, Tedge> rt, Tgate lastgate, Tgate finding)
+        {
+            var gateconn = lastgate;
+
+            if (gateconn.Equals(finding))
+            {
+                var newroute = new Route<Tgate, Tedge>(rt);
+                _totalRoute.Add(newroute);
+                return;
+            }
+
+            foreach (var nextedge in lastgate)
+            {
+                if (rt.Edges.Contains(nextedge))
+                {
+                    continue;
+                }
+
+                rt.Edges.Add(nextedge);
+                rt.Gates.Add(nextedge.NextGate);
+
+                recSearchLoop(rt, nextedge.NextGate, finding);
+
+                rt.Gates.Remove(nextedge.NextGate);
+                rt.Edges.Remove(nextedge);
+            }
+
+
+        }
+
+
+    }
+}
---
+++ ServerUtility/TestProject/EthClientTest.cs
@@ -0,0 +1,96 @@
+﻿using SensorLibrary.Eth;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using SensorLibrary;
+
+namespace TestProject
+{
+
+
+    /// <summary>
+    ///EthClientTest のテスト クラスです。すべての
+    ///EthClientTest 単体テストをここに含めます
+    ///</summary>
+    [TestClass()]
+    public class EthClientTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///現在のテストの実行についての情報および機能を
+        ///提供するテスト コンテキストを取得または設定します。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 追加のテスト属性
+        // 
+        //テストを作成するときに、次の追加属性を使用することができます:
+        //
+        //クラスの最初のテストを実行する前にコードを実行するには、ClassInitialize を使用
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //クラスのすべてのテストを実行した後にコードを実行するには、ClassCleanup を使用
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //各テストを実行する前にコードを実行するには、TestInitialize を使用
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //各テストを実行した後にコードを実行するには、TestCleanup を使用
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///Send のテスト
+        ///</summary>
+        [TestMethod()]
+        public void SendTest()
+        {
+            EthClient target = new EthClient();
+            target.Address = new IPAddress(new byte [] { 192, 168, 2, 24 });
+            EthPacket packet = new EthPacket()
+            {
+                srcId = new DeviceID(1, 0),
+                destId = new DeviceID(24, 0),
+            };
+
+            for (int i=0; i < packet.Data.Length; ++i)
+                packet.Data [i] = 0xCC;
+
+            for (int i = 0; i < 30000; ++i)
+            {
+                //packet.Message = string.Format("pero {0} times", i);
+
+                target.Send(packet);
+                System.Threading.Thread.Sleep(10);
+            }
+            Assert.Inconclusive("値を返さないメソッドは確認できません。");
+        }
+    }
+}
