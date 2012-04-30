ed897e6d8534d50069eea7c793c7e4043c03fa6e
sosoru <siva6991@yahoo.co.jp>
2012-04-30 22:00:25 +0900

    unload project of module_PointController, add new project of module_PointController by C++
    Signed-off-by: sosoru <siva6991@yahoo.co.jp>

---
+++ DeviceSoftware/TusDevice/module_PointController/module_PointController.cpp
@@ -0,0 +1,16 @@
+/*
+ * module_PointController.cpp
+ *
+ * Created: 2012/04/30 21:58:43
+ *  Author: Administrator
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
