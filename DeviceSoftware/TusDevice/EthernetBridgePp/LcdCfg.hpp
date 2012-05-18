2f4b0ef9c29ede6bc38ab00cd40bc19a6a2d0c1f
sosoru <siva6991@yahoo.co.jp>
2012-05-18 11:05:22 +0900

    merge commit
    Signed-off-by: sosoru <siva6991@yahoo.co.jp>

---
+++ DeviceSoftware/TusDevice/EthernetBridgePp/LcdCfg.hpp
@@ -0,0 +1,25 @@
+/*
+ * LcdCfg.hpp
+ *
+ * Created: 2012/05/18 10:53:42
+ *  Author: Administrator
+ */ 
+
+
+#ifndef LCDCFG_H_
+#define LCDCFG_H_
+
+#include "avr_base.hpp"
+#include "sc2004/sc2004.hpp"
+
+namespace EthernetBridge
+{
+	namespace Lcd
+	{
+		typedef Lcd_sc2004< PortC, OutputPin6<PortF>, OutputPin3<PortE>, OutputPin2<PortE> > Display;
+	}
+}
+
+
+
+#endif /* LCDCFG_H_ */
