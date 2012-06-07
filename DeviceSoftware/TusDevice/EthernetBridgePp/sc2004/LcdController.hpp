66cecf473ab7980a6946ba75c95febe6937b3b40
sosoru <siva6991@yahoo.co.jp>
2012-06-07 18:39:20 +0900

    nothing is chanded, commit for ide projct file

--- DeviceSoftware/TusDevice/EthernetBridgePp/sc2004/LcdController.hpp
+++ DeviceSoftware/TusDevice/EthernetBridgePp/sc2004/LcdController.hpp
@@ -9,6 +9,26 @@
 #ifndef LCDCONTROLLER_H_
 #define LCDCONTROLLER_H_
 
+namespace EthernetBridge {namespace Lcd {
+
+template<
+			class   t_display,
+			uint8_t t_width,
+			uint8_t t_height
+		>
+class LcdController
+{
+	static char* buf[t_width * t_height];
+	
+	template <uint8_t line>
+	static void WriteLine()
+	{
+		
+	}
+};
+
+}
+
 
 
 
