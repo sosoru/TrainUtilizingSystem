10b53a9678b69bebae56cbf620ee9664cea4f0a9
sosoru <siva6991@yahoo.co.jp>
2012-10-24 05:32:15 +0900

    [autosave] SensorLibrary/Devices/TusAvrDevices/KernelState.cs

--- ServerUtility/SensorLibrary/Devices/TusAvrDevices/KernelState.cs
+++ ServerUtility/SensorLibrary/Devices/TusAvrDevices/KernelState.cs
@@ -33,8 +33,8 @@ namespace SensorLibrary.Devices.TusAvrDevices
 
         public byte CurrentMemory
         {
-            get { return this.Data.ContentArray[0];}
-            set { this.Data.ContentArray[1];}
+            get { return this.Data.Content[0];}
+            set { this.Data.Content[1];}
         }
 
         
