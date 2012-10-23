ed5ddc879626ae4fb4461ab9708d714f24ee1ab1
sosoru <siva6991@yahoo.co.jp>
2012-10-24 03:40:53 +0900

    ditto

---
+++ ServerUtility/SensorLibrary/Devices/TusAvrDevices/Kernel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SensorLibrary.Packet.Control;
+using SensorLibrary.Packet.Data;
+
+namespace SensorLibrary.Devices.TusAvrDevices
+{
+    public class Kernel
+        : Device<KernelState>
+    {
+        public Kernel()
+            : base()
+        {
+            this.ModuleType = ModuleTypeEnum.AvrKernel;
+            this.CurrentState = new KernelState();
+        }
+
+        public Kernel(PacketServer serv)
+            : this()
+        {
+            this.CurrentState.ReceivingServer = serv;
+        }
+
+        public static Kernel InquiryState(DeviceID dev)
+        {
+            var k = new Kernel();
+
+            k.DeviceID = dev;
+            k.CurrentState.Command = KernelCommand.InquiryState;
+
+            return k;
+        }
+    }
+}
---
+++ ServerUtility/SensorLibrary/Devices/TusAvrDevices/KernelState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SensorLibrary.Packet.Data;
+
+namespace SensorLibrary.Devices.TusAvrDevices
+{
+    public class KernelState
+        : DeviceState<KernelData>
+    {
+        public KernelState()
+            : base()
+        {
+            this.Data = new KernelData();
+
+        }
+
+        public KernelCommand Command
+        {
+            get { return this.Data.Command; }
+            set { this.Data.Command = value; }
+        }
+    }
+}
