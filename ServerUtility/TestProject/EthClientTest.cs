eb90bdd6c81f912fad75c05c3c88d58d0e6752a1
sosoru <siva6991@yahoo.co.jp>
2012-10-27 03:47:10 +0900

    [autosave] TestProject/EthClientTest.cs

--- ServerUtility/TestProject/EthClientTest.cs
+++ ServerUtility/TestProject/EthClientTest.cs
@@ -156,7 +156,7 @@ namespace TestProject
             var mtrpacket = new EthPacket()
             {
                 srcId = new DeviceID(111, 0),
-                destId = new DeviceID(24, 1, 1),
+                destId = new DeviceID(24, 1, 17),
             };
             var mtr = new Motor() { DeviceID = mtrpacket.destId };
             var mtrstate = mtr.CurrentState;
