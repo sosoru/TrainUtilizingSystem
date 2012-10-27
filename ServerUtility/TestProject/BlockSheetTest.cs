591c4fb7f06a0b219f74be17f003eab1abd2b047
sosoru <siva6991@yahoo.co.jp>
2012-10-27 10:16:04 +0900

    [autosave] TestProject/BlockSheetTest.cs

--- ServerUtility/TestProject/BlockSheetTest.cs
+++ ServerUtility/TestProject/BlockSheetTest.cs
@@ -147,7 +147,8 @@ namespace TestProject
                 Speed = 0.5f
             };
 
-            //var existstate = new Sensor() { DeviceID = new DeviceID(1,2,
+            var existstate = new Sensor() { DeviceID = sht.GetBlock("AT4").Detector };
+            
 
             sht.Effect(cmd);
 
