9283b906740c895cdc339882cda3f362596e6b97
sosoru <siva6991@yahoo.co.jp>
2012-10-28 17:19:40 +0900

    [autosave] TestProject/EthClientTest.cs

--- ServerUtility/TestProject/EthClientTest.cs
+++ ServerUtility/TestProject/EthClientTest.cs
@@ -138,7 +138,7 @@ namespace TestProject
 
             ethpacket.DataPacket = DevicePacket.CreatePackedPacket(sens, inq).First();
             var states = target.AsyncSend(ethpacket)
-                .SelectMany(ob => Observable.Defer(() => target.AsyncReceive()))
+                .SelectMany(ob =>  target.AsyncReceive())
                 .Repeat(2)
                 .Do(pack =>
                     {
