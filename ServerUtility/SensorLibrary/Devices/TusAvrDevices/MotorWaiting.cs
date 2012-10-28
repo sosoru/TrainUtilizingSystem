8a21ab1155ba61dfc989f1c1d94180dfa5f2993f
sosoru <siva6991@yahoo.co.jp>
2012-10-28 18:33:08 +0900

    [autosave] SensorLibrary/Devices/TusAvrDevices/MotorWaiting.cs

---
+++ ServerUtility/SensorLibrary/Devices/TusAvrDevices/MotorWaiting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SensorLibrary;
+
+namespace SensorLibrary.Devices.TusAvrDevices
+{
+    public class MotorWaiting
+        : Device<MotorWaitingState>
+    {
+        public MotorWaiting()
+        {
+            this.CurrentState = new MotorWaitingState();
+        }
+
+
+    }
+}
