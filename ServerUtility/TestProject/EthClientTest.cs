d2ea62a24e0650e34eec07a139274c19944313b7
sosoru <siva6991@yahoo.co.jp>
2012-10-28 16:14:54 +0900

    [autosave] TestProject/EthClientTest.cs

--- ServerUtility/TestProject/EthClientTest.cs
+++ ServerUtility/TestProject/EthClientTest.cs
@@ -128,7 +128,7 @@ namespace TestProject
             };
             var sens = new Sensor() { DeviceID = ethpacket.destId };
             var sensstate = sens.CurrentState;
-            var sessetting = sens.CreateSettingDevice(4);
+            var sessetting = sens.CreateSettingDevice(2);
             var inq = Kernel.InquiryState(ethpacket.destId);
 
             ethpacket.DataPacket = DevicePacket.CreatePackedPacket(sessetting).First();
