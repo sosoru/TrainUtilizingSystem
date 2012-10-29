a34081450b7afe7195791c2fcf14b79f025af2d9
sosoru <siva6991@yahoo.co.jp>
2012-10-29 15:36:12 +0900

    [autosave] SensorLibrary/Devices/TusAvrDevices/Motor.cs

--- ServerUtility/SensorLibrary/Devices/TusAvrDevices/Motor.cs
+++ ServerUtility/SensorLibrary/Devices/TusAvrDevices/Motor.cs
@@ -28,5 +28,52 @@ namespace SensorLibrary.Devices.TusAvrDevices
                 return this.CurrentState.Current > this.CurrentState.ThresholdValue;
             }
         }
+
+        public IEnumerable<DevicePacket> ChangeMemoryTo(MotorMemoryStateEnum mem)
+        {
+            var mem = new MemoryState()
+            {
+                CurrentMemory = (byte)mem,
+            };
+
+            var kernel = Kernel.MemoryState(this.DeviceID, mem);
+            var devp = DevicePacket.CreatePackedPacket(kernel);
+
+            return new[] { devp };
+        }
+
+        public void SendStates()
+        {
+            var statelist = new List<IDevice<IDeviceState<IPacketDeviceData>>>();
+
+            
+        }
+
+        public MotorState StateWhenNoEffect
+        {
+            get;
+            set;
+        }
+
+        public MotorState StateWhenControlling
+        {
+            get;
+            set;
+        }
+
+        public MotorState StateWhenWaiting
+        {
+            get;
+            set;
+        }
+
+    }
+
+    public enum MotorMemoryStateEnum
+        : byte
+    {
+        NoEffect = 0x01,
+        Controlling = 0x02,
+        Waiting = 0x03,
     }
 }
