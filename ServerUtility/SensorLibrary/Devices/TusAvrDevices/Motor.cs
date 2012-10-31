7d2abe0c85feafed5fb43c3074f1a6972ff9e89c
sosoru <siva6991@yahoo.co.jp>
2012-10-31 17:42:13 +0900

    [autosave] SensorLibrary/Devices/TusAvrDevices/Motor.cs

--- ServerUtility/SensorLibrary/Devices/TusAvrDevices/Motor.cs
+++ ServerUtility/SensorLibrary/Devices/TusAvrDevices/Motor.cs
@@ -91,11 +91,11 @@ namespace SensorLibrary.Devices.TusAvrDevices
 
             var app = this.CreateApplyingStates();
             foreach (var p in app)
-                this.SendPacket(p);
+                this.ReceivingServer.SendPacket(p);
 
             var pack = this.ChangeMemoryTo(this.CurrentMemory);
             foreach (var p in pack)
-                this.SendPacket(p);
+                this.ReceivingServer.SendPacket(p);
         }
 
         public MotorMemoryStateEnum CurrentMemory
