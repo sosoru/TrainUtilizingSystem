83439717472d9d7e871984b56200e5810f1deda7
sosoru <siva6991@yahoo.co.jp>
2012-10-31 15:34:22 +0900

    [autosave] SensorLibrary/Devices/BasicDevices/KernelState.cs

---
+++ ServerUtility/SensorLibrary/Devices/BasicDevices/KernelState.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SensorLibrary.Packet.Data;
+
+namespace SensorLibrary.Devices
+{
+    public class KernelState
+        : DeviceState<KernelData>
+    {
+        public KernelState()
+            : base()
+        {
+            this.Data = new KernelData();
+
+        }
+
+        public KernelCommand Command
+        {
+            get { return this.Data.Command; }
+            set { this.Data.Command = value; }
+        }
+    }
+
+    public class InquiryState
+        : KernelState
+    {
+        public InquiryState()
+            : base()
+        {
+            this.Command = KernelCommand.InquiryState;
+        }
+    }
+
+    public class MemoryState
+        : KernelState
+    {
+        public MemoryState()
+            : base()
+        {
+            this.Command = KernelCommand.MemoryState;
+        }
+
+        public MemoryState(int mem)
+            : this()
+        {
+            this.CurrentMemory = (byte)mem;
+        }
+
+        public byte CurrentMemory
+        {
+            get { return this.Data.Content[0]; }
+            set { this.Data.Content[0] = value; }
+        }
+
+        public byte MemoryLimit
+        {
+            get { return this.Data.Content[1]; }
+            set { this.Data.Content[1] = value; }
+        }
+   }
+
+}
