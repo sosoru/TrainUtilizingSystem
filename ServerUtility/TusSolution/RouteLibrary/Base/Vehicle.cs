9e31273dd525cc0363ecac0a7401abd95341900c
sosoru <siva6991@yahoo.co.jp>
2012-10-31 16:48:57 +0900

    [autosave] TusSolution/RouteLibrary/Base/Vehicle.cs

--- ServerUtility/TusSolution/RouteLibrary/Base/Vehicle.cs
+++ ServerUtility/TusSolution/RouteLibrary/Base/Vehicle.cs
@@ -5,32 +5,13 @@ using System.Text;
 
 namespace RouteLibrary.Base
 {
-    public class VehicleState
-    {
-        public Block Position { get; set; }
-    }
 
     public class Vehicle
-        : IObserver<VehicleState>
     {
-        public Route Route;
+        public Block CurrentBlock { get; set; }
+        public Route Route { get; set; }
 
         public Vehicle() { }
 
-
-        public void OnCompleted()
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnError(Exception error)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void OnNext(VehicleState value)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
