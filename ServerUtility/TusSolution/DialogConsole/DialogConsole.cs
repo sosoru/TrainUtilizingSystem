9c5621b9598c748e37c01d563900f2b4ebce5623
sosoru <siva6991@yahoo.co.jp>
2012-11-04 07:59:13 +0900

    [autosave] TusSolution/DialogConsole/DialogConsole.cs

--- ServerUtility/TusSolution/DialogConsole/DialogConsole.cs
+++ ServerUtility/TusSolution/DialogConsole/DialogConsole.cs
@@ -10,6 +10,7 @@ using SensorLibrary;
 using SensorLibrary.Packet;
 using SensorLibrary.Packet.Control;
 using SensorLibrary.Devices;
+using SensorLibrary.Devices.TusAvrDevices;
 
 using RouteLibrary;
 using RouteLibrary.Base;
@@ -24,7 +25,41 @@ namespace DialogConsole
             var serv = CreateServer(IPAddress.Parse("192.168.2.0"), IPAddress.Parse("255.255.255.0"));
             var sht = CreateSheet("layout.yaml", serv);
 
-            
+
+
+        }
+
+        static void ChangeSwitchAll(BlockSheet sht,  PointStateEnum pos)
+        {
+            sht.InquiryAllMotors
+        }
+
+        static void Detect(BlockSheet sht)
+        {
+            sht.ChangeDetectingMode();
+
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine(sht.InnerBlocks
+               .Where(b => b.IsDetectingTrain)
+                .Aggregate("", (ac, b) => ac += b.Name + ", "));
+        }
+
+        static Route InputRoute(BlockSheet sht)
+        {
+            Console.WriteLine("route?");
+            var content = Console.ReadLine();
+
+            try
+            {
+                var spil = content.Split(',');
+                var rt = new Route(sht, spil.Select(s => sht.GetBlock(s)));
+
+                return rt;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         static PacketServer CreateServer(IPAddress ipbase, IPAddress ipmask)
@@ -59,22 +94,22 @@ namespace DialogConsole
         private PacketDispatcher dispat;
         private DeviceID devid;
 
-        public Stream Writer { get; private set;}
-        public Stream Reader { get; private set;}
+        public Stream Writer { get; private set; }
+        public Stream Reader { get; private set; }
 
         public DeviceDiagnostics(DeviceID id, IPAddress baseip)
         {
-            this.devid = new DeviceID(baseip.GetAddressBytes()[3], 0,0);
-            var io = new SensorLibrary.Packet.IO.TusEthernetIO(baseip, new IPAddress(new byte[]{255,255,255,0}))
+            this.devid = new DeviceID(baseip.GetAddressBytes()[3], 0, 0);
+            var io = new SensorLibrary.Packet.IO.TusEthernetIO(baseip, new IPAddress(new byte[] { 255, 255, 255, 0 }))
             {
                 SourceID = devid,
                 Port = 8000,
             };
-            this.serv  = new PacketServer(new AvrDeviceFactoryProvider())
+            this.serv = new PacketServer(new AvrDeviceFactoryProvider())
                 {
                     Controller = io,
                 };
-            
+
             this.dispat = new PacketDispatcher();
             this.serv.AddAction(this.dispat);
 
@@ -83,11 +118,11 @@ namespace DialogConsole
         public void Start(Stream reader, Stream writer)
         {
             this.serv.LoopStart();
-            
-            if(!reader.CanRead)
+
+            if (!reader.CanRead)
                 throw new InvalidOperationException("requires readable stream");
 
-            if(!writer.CanWrite)
+            if (!writer.CanWrite)
                 throw new InvalidOperationException("requires writable stream");
 
             this.Reader = reader;
@@ -96,14 +131,14 @@ namespace DialogConsole
         }
 
         public TDev CreateDevice<TDev>(DeviceID devid)
-            where TDev : Device<IDeviceState<IPacketDeviceData>> ,new()
+            where TDev : Device<IDeviceState<IPacketDeviceData>>, new()
         {
-            var dev = new TDev() { DeviceID = devid};
+            var dev = new TDev() { DeviceID = devid };
             dev.ReceivingServer = this.serv;
             dev.Observe(this.dispat);
 
             return dev;
         }
-        
+
     }
 }
