f98e36808fcfa1e78275593f3e541354c135f9a8
sosoru <siva6991@yahoo.co.jp>
2012-11-06 15:38:24 +0900

    [autosave] TusSolution/RouteLibrary/Base/BlockSheet.cs

--- ServerUtility/TusSolution/RouteLibrary/Base/BlockSheet.cs
+++ ServerUtility/TusSolution/RouteLibrary/Base/BlockSheet.cs
@@ -176,5 +176,10 @@ namespace RouteLibrary.Base
 
             this.Vehicles = g;
         }
+
+        public IEnumerable<Block> GetBlocks(string p1, string p2, string p3, string p4, string p5)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
