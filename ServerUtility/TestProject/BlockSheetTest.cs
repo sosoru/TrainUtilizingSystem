3092a3e6da690ba486b72b984ade9ddccbb6fbb0
sosoru <siva6991@yahoo.co.jp>
2012-11-12 03:44:05 +0900

    [autosave] TestProject/BlockSheetTest.cs

--- ServerUtility/TestProject/BlockSheetTest.cs
+++ ServerUtility/TestProject/BlockSheetTest.cs
@@ -34,6 +34,12 @@ namespace TestProject
             return (TCast) list[id];
         }
 
+        public static TCast ExtractDevice<TCast>(this IEnumerable<IDeviceState<IPacketDeviceData>> list, ushort parent, byte module, byte inter)
+        {
+            var id = new DeviceID(parent, module, inter);
+            return (TCast)list.First(p => p.ID == id);
+        }
+
     }
 
 
