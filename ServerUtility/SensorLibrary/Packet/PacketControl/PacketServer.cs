e4ee1ccdce712f00ea4bcbdf51ee1c64817a89f0
sosoru <siva6991@yahoo.co.jp>
2012-11-12 05:35:25 +0900

    [autosave] SensorLibrary/Packet/PacketControl/PacketServer.cs

--- ServerUtility/SensorLibrary/Packet/PacketControl/PacketServer.cs
+++ ServerUtility/SensorLibrary/Packet/PacketControl/PacketServer.cs
@@ -120,9 +120,11 @@ namespace SensorLibrary.Packet.Control
 
         private IObservable<DevicePacket> SendState()
         {
-            return Observable.Return(((this.sending_queue.Count > 0) ? this.sending_queue.Dequeue() : null))
-                                        .Delay(TimeSpan.FromMilliseconds(1))
-                                        .SkipWhile(d => d == null);
+            return Observable.Create(this.sending_queue.ToArray())
+                .Where(d => d != null);
+            //return Observable.Return(((this.sending_queue.Count > 0) ? this.sending_queue.Dequeue() : null))
+            //                            .Delay(TimeSpan.FromMilliseconds(1))
+            //                            .SkipWhile(d => d == null);
         }
 
         public IObservable<IDeviceState<IPacketDeviceData>> ReceivingObservable
