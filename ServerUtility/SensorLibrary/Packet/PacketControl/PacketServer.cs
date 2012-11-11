0db766b0bb465cdcc6bd4e80e1e51cd3e8468e89
sosoru <siva6991@yahoo.co.jp>
2012-11-12 05:37:51 +0900

    [autosave] SensorLibrary/Packet/PacketControl/PacketServer.cs

--- ServerUtility/SensorLibrary/Packet/PacketControl/PacketServer.cs
+++ ServerUtility/SensorLibrary/Packet/PacketControl/PacketServer.cs
@@ -121,8 +121,7 @@ namespace SensorLibrary.Packet.Control
         private IObservable<DevicePacket> SendState()
         {
             return this.sending_queue.ToArray()
-                .ToObservable()
-                .SelectMany(a => a);
+                .ToObservable();
             //return Observable.Return(((this.sending_queue.Count > 0) ? this.sending_queue.Dequeue() : null))
             //                            .Delay(TimeSpan.FromMilliseconds(1))
             //                            .SkipWhile(d => d == null);
