a4a5f93c859b007508a4f2bb3c9118f7614707cb
sosoru <siva6991@yahoo.co.jp>
2012-11-14 18:50:36 +0900

    [autosave] TestProject/PacketServerTest.cs

---
+++ ServerUtility/TestProject/PacketServerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class PacketServerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
