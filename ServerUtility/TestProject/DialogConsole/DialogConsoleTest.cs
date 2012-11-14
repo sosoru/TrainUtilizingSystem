fffc8cd36ecf16081e9c35629694745d48d048fa
sosoru <siva6991@yahoo.co.jp>
2012-11-14 15:43:19 +0900

    [autosave] TestProject/DialogConsole/DialogConsoleTest.cs

---
+++ ServerUtility/TestProject/DialogConsole/DialogConsoleTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DialogConsole;
+
+namespace TestProject.DialogConsole
+{
+    [TestClass]
+    public class DialogConsoleTest
+    {
+        [TestMethod]
+        public void 
+        {
+        }
+    }
+}
