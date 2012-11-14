8664d68af3139f159c6ff1449f058d5449224f3d
sosoru <siva6991@yahoo.co.jp>
2012-11-14 20:07:57 +0900

    [autosave] TestProject/PacketServerTest.cs

--- ServerUtility/TestProject/PacketServerTest.cs
+++ ServerUtility/TestProject/PacketServerTest.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using SensorLibrary.Packet;
+using SensorLibrary.Packet.Data;
+using SensorLibrary.Packet.IO;
+using SensorLibrary.Packet.Control;
+using SensorLibrary.Devices;
+using SensorLibrary.Devices.TusAvrDevices;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Concurrency;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using System.Linq;
+using SensorLibrary;
+using SensorLibrary.Devices;
+using Moq;
+using System.Collections.Generic;
+using Microsoft.Reactive.Testing;
 
 namespace TestProject
 {
     [TestClass]
     public class PacketServerTest
     {
+        private PacketServer Server { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            this.Server = new PacketServer(new AvrDeviceFactoryProvider());
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
