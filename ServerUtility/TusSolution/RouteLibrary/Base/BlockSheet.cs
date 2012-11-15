eaa6972156e36d9f06c73fa58eddb23e555ed9ba
sosoru <siva6991@yahoo.co.jp>
2012-11-16 06:51:56 +0900

    [autosave] TusSolution/RouteLibrary/Base/BlockSheet.cs

--- ServerUtility/TusSolution/RouteLibrary/Base/BlockSheet.cs
+++ ServerUtility/TusSolution/RouteLibrary/Base/BlockSheet.cs
@@ -98,7 +98,7 @@ namespace RouteLibrary.Base
             var arrays = GetEffectObservable(cmd, blocks);
 
             return Observable.Timer(DateTimeOffset.MinValue, TimeSpan.FromSeconds(5), this.AssociatedScheduler)
-                .Select((l, i) => new { l ,i })
+                .Select((l, i) => new { l ,i-1 })
                 .Subscribe(val => arrays[val.i].ForEach(e => e.ExecuteCommand()));
         }
 
