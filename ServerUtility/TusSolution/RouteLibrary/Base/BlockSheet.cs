19ede0de417af8660b4b1361e27aa00c2e378266
sosoru <siva6991@yahoo.co.jp>
2012-11-16 05:11:15 +0900

    [autosave] TusSolution/RouteLibrary/Base/BlockSheet.cs

--- ServerUtility/TusSolution/RouteLibrary/Base/BlockSheet.cs
+++ ServerUtility/TusSolution/RouteLibrary/Base/BlockSheet.cs
@@ -96,7 +96,7 @@ namespace RouteLibrary.Base
         {
             GetEffectObservable(cmd, blocks)
                 .ObserveOn(this.AssociatedScheduler)
-                .SubscribeOn(Scheduler.ThreadPool)
+                .SubscribeOn(Scheduler.CurrentThread)
                 .Subscribe();
         }
 
