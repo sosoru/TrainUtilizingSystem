0bc5a4ce27c959a3264e66b57948021129b616dc
sosoru <siva6991@yahoo.co.jp>
2012-11-16 04:38:38 +0900

    [autosave] TestProject/RouteTest/StaLoopTest.cs

--- ServerUtility/TestProject/RouteTest/StaLoopTest.cs
+++ ServerUtility/TestProject/RouteTest/StaLoopTest.cs
@@ -159,7 +159,7 @@ namespace TestProject
             scheduler.Schedule(vh.Run);
 
             scheduler.Start();
-            scheduler.AdvanceBy(TimeSpan.FromSeconds(0.1).Ticks);
+            scheduler.AdvanceBy(TimeSpan.FromSeconds(5.1).Ticks);
             serv.SendingObservable.Subscribe();
 
             Assert.IsTrue(written.ExtractDevice<SwitchState>(1, 1, 1).Position == PointStateEnum.Straight);
