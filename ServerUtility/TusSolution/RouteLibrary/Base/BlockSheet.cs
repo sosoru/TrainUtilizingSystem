976cba14b6ea4162d2c6dc1c2b6f54c71659dae3
sosoru <siva6991@yahoo.co.jp>
2012-11-16 19:24:02 +0900

    [autosave] TusSolution/RouteLibrary/Base/BlockSheet.cs

--- ServerUtility/TusSolution/RouteLibrary/Base/BlockSheet.cs
+++ ServerUtility/TusSolution/RouteLibrary/Base/BlockSheet.cs
@@ -122,7 +122,7 @@ namespace RouteLibrary.Base
 
         public void ChangeDetectingMode()
         {
-            var detectionduty = 1.f;
+            var detectionduty = 1.0f;
 
             this.InnerBlocks.Where(b => b.HasMotor)
                 .ToObservable()
