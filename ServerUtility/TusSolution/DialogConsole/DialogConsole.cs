c62862d7e37669004fe33ff48d5c806b313fc1fe
sosoru <siva6991@yahoo.co.jp>
2012-11-17 11:52:58 +0900

    [autosave] TusSolution/DialogConsole/DialogConsole.cs

--- ServerUtility/TusSolution/DialogConsole/DialogConsole.cs
+++ ServerUtility/TusSolution/DialogConsole/DialogConsole.cs
@@ -292,14 +292,24 @@ namespace DialogConsole
         public void VehicleProcess()
         {
             this.Sheet.InquiryAllMotors();
-
             var detected = this.Sheet.InnerBlocks.Where(b => b.IsMotorDetectingTrain);
             var vehicle = this.Vehicles.First();
+
+            if (detected.Count() == 0)
+            {
+                vehicle.Length++;
+            }
 
             var movedfor = detected.Except(new[] { vehicle.CurrentBlock }).ToArray();
 
             if (movedfor.Length > 0)
+            {
                 vehicle.CurrentBlock = movedfor.First();
+                vehicle.Length--;
+
+                if (vehicle.Length == 0)
+                    vehicle.Length = 1;
+            }
 
             foreach (var v in this.Vehicles)
             {
