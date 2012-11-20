8681d5627de165374fc97ce030252a819e8fa2ca
sosoru <siva6991@yahoo.co.jp>
2012-11-20 18:58:23 +0900

    [autosave] TusSolution/DialogConsole/DialogConsole.cs

--- ServerUtility/TusSolution/DialogConsole/DialogConsole.cs
+++ ServerUtility/TusSolution/DialogConsole/DialogConsole.cs
@@ -103,12 +103,12 @@ namespace DialogConsole
             this.Server.SendingObservable
                 .Delay(TimeSpan.FromMilliseconds(15))
                 .Repeat()
-                //.SelectMany(g => g.ExtractPackedPacket())
-                //.Do(g => Console.WriteLine(string.Format("({0}.{1}) : sending {2}",
-                //                    DateTime.Now.ToLongTimeString(),
-                //                    DateTime.Now.Millisecond,
-                //                    g.ToString()
-                //                    )))
+                .SelectMany(g => g.ExtractPackedPacket())
+                .Do(g => Console.WriteLine(string.Format("({0}.{1}) : sending {2}",
+                                    DateTime.Now.ToLongTimeString(),
+                                    DateTime.Now.Millisecond,
+                                    g.ToString()
+                                    )))
                 .ObserveOn(this.SchedulerSendingProcessing)
                 .SubscribeOn(Scheduler.NewThread)
                 .Subscribe();
@@ -118,12 +118,12 @@ namespace DialogConsole
                 .ObserveOn(this.SchedulerSendingProcessing)
                 .Repeat()
                 .Zip(timer, (v, _) => v)
-                //.SelectMany( v => v.ExtractPackedPacket())
-                //.Do(g => Console.WriteLine(string.Format("({0}.{1}) : recving {2}",
-                //                                                    DateTime.Now.ToLongTimeString(),
-                //                                                    DateTime.Now.Millisecond,
-                //                                                    g.ToString()
-                //                                                    )))
+                .SelectMany(v => v.ExtractPackedPacket())
+                .Do(g => Console.WriteLine(string.Format("({0}.{1}) : recving {2}",
+                                                                    DateTime.Now.ToLongTimeString(),
+                                                                    DateTime.Now.Millisecond,
+                                                                    g.ToString()
+                                                                    )))
                                                             .SubscribeOn(Scheduler.NewThread)
                                                             .Subscribe();
             this.ServingInfomation_ = Observable.Defer(() => this.GetHttpObservable())
