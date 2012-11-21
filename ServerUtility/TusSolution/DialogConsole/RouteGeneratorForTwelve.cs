dd9697cbf4c029194efd436fa861f5379d8aaad7
sosoru <siva6991@yahoo.co.jp>
2012-11-22 06:52:26 +0900

    [autosave] TusSolution/DialogConsole/RouteGeneratorForTwelve.cs

--- ServerUtility/TusSolution/DialogConsole/RouteGeneratorForTwelve.cs
+++ ServerUtility/TusSolution/DialogConsole/RouteGeneratorForTwelve.cs
@@ -43,5 +43,30 @@ namespace DialogConsole
 
             return list;
         }
+
+        public static IEnumerable<string> GetLoopB(bool inv, bool sub)
+        {
+            var list = new List<string>();
+            list.AddRange(new[] { "BT1", "BBT1", });
+
+            if (sub)
+                list.AddRange(new[] { "BT3", "BT4S", "BT5S", "BT5-1", "BT6", });
+            else
+                list.AddRange(new[] { "BT3", "BT4", "BT5", "BT6" });
+
+            list.AddRange(new[] { "BBT6", "BT7", "BBT7", "BT8", "BBT8", "BT9", "BBT9", "BT10P", "BT10", "BBT10", "BT11", "BBT11", });
+
+            if (inv)
+            {
+                var lastisolate = list.Last();
+                list.RemoveAt(list.Count - 1);
+
+                list.Reverse();
+                list.Add(lastisolate);
+            }
+
+            return list;
+        }
+
     }
 }
