299f57246d2ec80b55f743dde481c61e9e8022ac
sosoru <siva6991@yahoo.co.jp>
2012-11-22 07:23:07 +0900

    [autosave] TusSolution/DialogConsole/RouteGeneratorForTwelve.cs

--- ServerUtility/TusSolution/DialogConsole/RouteGeneratorForTwelve.cs
+++ ServerUtility/TusSolution/DialogConsole/RouteGeneratorForTwelve.cs
@@ -56,50 +56,31 @@ namespace DialogConsole
         }
         public static IEnumerable<string> GetLoopA(bool inv, bool sub)
         {
-            var list = new List<string>();
-            list.AddRange(new[] { "AT1", "BAT1", });
-
-            if (sub)
-                list.AddRange(new[] { "AT3", "AT4S", "AT5S", "AT5-1", "AT6" });
-            else
-                list.AddRange(new[] { "AT3", "AT4", "AT5", "AT6" });
-
-            list.AddRange(new[] { "BAT6", "AT7", "BAT7", "AT8", "BAT8", "AT9", "BAT9", "AT10P", "AT10", "BAT10", "AT11", "BAT11" });
-
-            if (inv)
+            var gen = new RouteGeneratorForTwelve()
             {
-                var lastisolate = list.Last();
-                list.RemoveAt(list.Count - 1);
+                Reverse = inv,
+                UseSubline = sub,
+                KwToAb = new[] { "AT1", "BAT1", },
+                AbMain = new[] { "AT3", "AT4", "AT5", "AT6" },
+                AbSub = new[] { "AT3", "AT4S", "AT5S", "AT5-1", "AT6" },
+                AbToKw = new[] { "BAT6", "AT7", "BAT7", "AT8", "BAT8", "AT9", "BAT9", "AT10P", "AT10", "BAT10", "AT11", "BAT11" },
+            };
 
-                list.Reverse();
-                list.Add(lastisolate);
-            }
-
-            return list;
+            return gen.GetLoop();
         }
 
         public static IEnumerable<string> GetLoopB(bool inv, bool sub)
         {
-            var list = new List<string>();
-            list.AddRange(new[] { "BT1", "BBT1", });
-
-            if (sub)
-                list.AddRange(new[] { "BT3", "BT4S", "BT5S", "BT5-1", "BT6", });
-            else
-                list.AddRange(new[] { "BT3", "BT4", "BT5", "BT6" });
-
-            list.AddRange(new[] { "BBT6", "BT7", "BBT7", "BT8", "BBT8", "BT9", "BBT9", "BT10P", "BT10", "BBT10", "BT11", "BBT11", });
-
-            if (inv)
+            var gen = new RouteGeneratorForTwelve()
             {
-                var lastisolate = list.Last();
-                list.RemoveAt(list.Count - 1);
-
-                list.Reverse();
-                list.Add(lastisolate);
-            }
-
-            return list;
+                Reverse = inv,
+                UseSubline = sub,
+                KwToAb = new[] { "BT1", "BBT1", },
+                AbMain = new[] { "BT3", "BT4", "BT5", "BT6" },
+                AbSub = new[] { "BT3", "BT4S", "BT5S", "BT5-1", "BT6", },
+                AbToKw = new[] { "BBT6", "BT7", "BBT7", "BT8", "BBT8", "BT9", "BBT9", "BT10P", "BT10", "BBT10", "BT11", "BBT11", },
+            };
+            return gen.GetLoop();
         }
 
         public static IEnumerable<string> GetLoopC(bool inv, bool sub)
