a152e459f605f3d151c1f77cbd01553e31033b6d
sosoru <siva6991@yahoo.co.jp>
2012-11-23 17:22:34 +0900

    [autosave] TusSolution/RouteLibrary/Base/DeviceEffector.cs

--- ServerUtility/TusSolution/RouteLibrary/Base/DeviceEffector.cs
+++ ServerUtility/TusSolution/RouteLibrary/Base/DeviceEffector.cs
@@ -101,7 +101,7 @@ namespace RouteLibrary.Base
                 MemoryWhenEntered = MotorMemoryStateEnum.Controlling,
                 DestinationID = beforemtr.DeviceID,
                 DestinationMemory = MotorMemoryStateEnum.Locked,
-                ThresholdCurrent = 1.5f,
+                ThresholdCurrent = 0.5f,
             };
 
             return state;
