a410d1bae9595ab18258b0711eaa63f60a37a43c
sosoru <siva6991@yahoo.co.jp>
2012-11-24 19:01:22 +0900

    [autosave] TestProject/EthClientTest.cs

--- ServerUtility/TestProject/EthClientTest.cs
+++ ServerUtility/TestProject/EthClientTest.cs
@@ -377,7 +377,7 @@ namespace TestProject
             var ptpacket = new EthPacket()
             {
                 srcId = new DeviceID(9, 0),
-                destId = new DeviceID(25, 5, 1),
+                destId = new DeviceID(25, 1, 1),
             };
 
             var pt = new Switch();
