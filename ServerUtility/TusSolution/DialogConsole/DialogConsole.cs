3c64c0bbc0405a9c1329d9f7c5f80fd48ef9a6d8
sosoru <siva6991@yahoo.co.jp>
2012-11-24 12:51:55 +0900

    [autosave] TusSolution/DialogConsole/DialogConsole.cs

--- ServerUtility/TusSolution/DialogConsole/DialogConsole.cs
+++ ServerUtility/TusSolution/DialogConsole/DialogConsole.cs
@@ -253,7 +253,7 @@ this.Sheet.InnerBlocks);
 
             this.VehicleProcessing_ = Observable.Defer(() => Observable.Start(VehicleProcess, this.SchedulerSendingProcessing))
                 .Do(u => this.Sheet.InquiryAllMotors())
-                .Delay(TimeSpan.FromMilliseconds(500))
+                .Delay(TimeSpan.FromMilliseconds(1100))
 
                 .Repeat()
                 .SubscribeOn(Scheduler.NewThread)
