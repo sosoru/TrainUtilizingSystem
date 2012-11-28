2aa1959f8998ed46fccaa19ab6dc0c9fffe04ccb
sosoru <siva6991@yahoo.co.jp>
2012-11-28 18:11:09 +0900

    [autosave] SensorLibrary/Packet/PacketIO/EthClient.cs

--- ServerUtility/SensorLibrary/Packet/PacketIO/EthClient.cs
+++ ServerUtility/SensorLibrary/Packet/PacketIO/EthClient.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 using System.Reactive;
 using System.Reactive.Linq;
