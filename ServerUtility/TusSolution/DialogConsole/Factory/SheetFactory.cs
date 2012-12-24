c09698de9f07b329226e2b7383357d033ad5ec42
sosoru <siva6991@yahoo.co.jp>
2012-12-25 03:45:01 +0900

    [autosave] TusSolution/DialogConsole/Factory/SheetFactory.cs

--- ServerUtility/TusSolution/DialogConsole/Factory/SheetFactory.cs
+++ ServerUtility/TusSolution/DialogConsole/Factory/SheetFactory.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.Composition;
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestProject.ConsoleTest")]
 
 namespace DialogConsole.Factory
 {
+    [Export]
     class SheetFactory
     {
         public string Path { get; set; }
+
+        [Import]
         public ServerFactory ServerCreater { get; set; }
     }
 
