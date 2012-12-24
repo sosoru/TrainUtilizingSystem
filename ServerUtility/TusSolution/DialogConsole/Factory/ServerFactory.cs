fddfc49d488449f36b5fbbf4e7a3956df2b93335
sosoru <siva6991@yahoo.co.jp>
2012-12-25 02:07:56 +0900

    [autosave] TusSolution/DialogConsole/Factory/ServerFactory.cs

---
+++ ServerUtility/TusSolution/DialogConsole/Factory/ServerFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestProject.ConsoleTest")]
+
+namespace DialogConsole.Factory
+{
+    class ServerFactory
+    {
+    }
+}
