4cb7c300d131f02eea463c9e2ccfd858a8b83833
sosoru <siva6991@yahoo.co.jp>
2012-12-25 09:22:28 +0900

    [autosave] TestProject/ConsoleTest/Composition/SheetFactoryTest.cs

---
+++ ServerUtility/TestProject/ConsoleTest/Composition/SheetFactoryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DialogConsole;
+using DialogConsole.Factory;
+
+namespace TestProject.ConsoleTest.Composition
+{
+    [TestClass]
+    public class SheetFactoryTest 
+    {
+        [TestMethod]
+        public void CreateSheet()
+       {
+           var path = "dummy";
+           var f = new SheetFactory(path);
+
+        }
+    }
+}
