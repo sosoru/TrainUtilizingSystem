f3a51af2d2ed1fae34d6590fe770bf9bdd411f08
sosoru <siva6991@yahoo.co.jp>
2012-12-27 09:23:31 +0900

    [autosave] TusSolution/DialogConsole/Settings.cs

--- ServerUtility/TusSolution/DialogConsole/Settings.cs
+++ ServerUtility/TusSolution/DialogConsole/Settings.cs
@@ -6,7 +6,7 @@
     //  PropertyChanged イベントは、設定値が変更された後に発生します。
     //  SettingsLoaded イベントは、設定値が読み込まれた後に発生します。
     //  SettingsSaving イベントは、設定値が保存される前に発生します。
-    [Export]
+    [Export(typeof(IConsoleApplicationSettings)]
     internal sealed partial class Settings
         : DialogConsole.IConsoleApplicationSettings {
         
