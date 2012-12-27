bfbbff784939987468c4d9401e6a2064d9962258
sosoru <siva6991@yahoo.co.jp>
2012-12-27 09:28:36 +0900

    [autosave] TusSolution/DialogConsole/Factory/IConsoleApplicationSettings.cs

---
+++ ServerUtility/TusSolution/DialogConsole/Factory/IConsoleApplicationSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace DialogConsole.Factory
+{
+    interface IConsoleApplicationSettings
+    {
+        string IpMask { get; set; }
+        int IpPort { get; set; }
+        string IpSegment { get; set; }
+        string ParentDeviceID { get; set; }
+        string SheetPath { get; set; }
+    }
+}
