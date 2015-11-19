66e78905a5cc1ed22771b0950cd2522dee172631
sosoru <siva6991@yahoo.co.jp>
2015-11-19 12:51:33 +0900

    add lacked file while merging

---
+++ ServerUtility/TusSolution/DialogConsole/Features/RemoveVehicleFeature.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using DialogConsole.Features.Base;
+
+namespace DialogConsole.Features
+{
+    [FeatureMetadata("6", "remove vehicle")]
+    [Export(typeof(IFeature))]
+    class RemoveVehicleFeature
+        : BaseFeature, IFeature
+    {
+        public void Execute()
+        {
+            Console.WriteLine("type name? ");
+            var name = Console.ReadLine();
+
+            var v = this.Param.UsingLayout.Vehicles.FirstOrDefault(b => b.Name == name);
+
+            if (v == null)
+            {
+                Console.WriteLine("not found");
+                return;
+            }
+
+            v.Length = 1;
+            v.Run(0);
+            v.AssociatedRoute.UnReserveHead();
+            v.AssociatedRoute.InitLockingPosition();
+
+            this.Param.UsingLayout.Vehicles.Remove(v);
+        }
+
+        public void Init()
+        {
+        }
+    }
+}
